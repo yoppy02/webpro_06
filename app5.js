a866a1c8eecf93d9a4a50882b2e31ce572640124
Yoppy02 <yoyuudayo1@gmail.com>
2024-11-01 15:15:36 +0900

    コメント

--- app5.js
+++ app5.js
@@ -29,29 +29,36 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  let lose = Number( req.query.lose );
-  console.log( {hand, win, total, lose} );
+  let win =  0|Number( req.query.win);
+  let total = 0|Number( req.query.total );
+  let lose = 0|Number( req.query.lose);
+  var all =0|Number( req.query.all );
+  console.log( {hand, win, total, lose, all} );
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  if ( hand == 'パー' && mun==1,hand == 'チョキ' && num==3, hand == 'グー' && num==2)judgement='勝ち';
-  else if (hand == 'グー' && num==1, hand == 'パー' && num==3, hand == 'チョキ' && num==2)judgement='あいこ';
-  else judgement='負け';
-  
-  if (judgement == '勝ち')win += 1, total += 1;
-  else if (judgement == 'あいこ') total += 1;
-  else (judgement == '負け') lose +=1,total+=1;
+  if (all==0) cpu=' ';
+  else;
+  console.log( {cpu} );
+  if ( hand == 'パー' && num==1 || hand == 'チョキ' && num==3 || hand == 'グー' && num==2)judgement='勝ち';
+  else if (hand == 'グー' && num==1 || hand == 'パー' && num==3 || hand == 'チョキ' && num==2)judgement='あいこ';
+  else if (hand == 'チョキ' && num==1 || hand == 'グー' && num==3 || hand == 'パー' && num==2)judgement='負け';
+  else judgement = '';
+
+  if (judgement == '勝ち') win += 1,total += 1;
+  else if (judgement == 'あいこ');
+  else if (judgement == '負け') lose +=1,total += 1;
+  all += 1;
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
-    total: total
+    total: total,
+    lose: lose,
+    all: all
   };
   
   
